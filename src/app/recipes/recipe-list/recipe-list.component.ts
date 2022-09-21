3e8ef2c4ae5d4fa43a2a1356240bc30fa819f9cd
GrazielyTukozaki <grazielybasilio@hotmail.com>
2022-09-21 15:11:36 -0300

    Add basic styling to Recipe Area

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test Recipe',
+      'This is a test',
+      'https://cdn.pixabay.com/photo/2018/03/06/09/02/food-3202918_1280.jpg'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
